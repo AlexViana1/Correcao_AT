bb565be3c423246fefdd517664c8e01e4a1f28e9
AlexViana1 <42471819+AlexViana1@users.noreply.github.com>
2018-08-17 11:29:37 -0300

    Initial commit

---
+++ CorrecaDaProva/CorrecaDaProva/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CorrecaDaProva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbRodada = new System.Windows.Forms.Label();
+            this.lbVitorias = new System.Windows.Forms.Label();
+            this.lbEmpates = new System.Windows.Forms.Label();
+            this.btPedra = new System.Windows.Forms.Button();
+            this.btPapel = new System.Windows.Forms.Button();
+            this.btTesoura = new System.Windows.Forms.Button();
+            this.rbResultado = new System.Windows.Forms.RichTextBox();
+            this.btJogar = new System.Windows.Forms.Button();
+            this.btDescelecionar = new System.Windows.Forms.Button();
+            this.btReinicar = new System.Windows.Forms.Button();
+            this.lbJogadaSelecionada = new System.Windows.Forms.Label();
+            this.lbDerrotas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbRodada
+            // 
+            this.lbRodada.AutoSize = true;
+            this.lbRodada.Location = new System.Drawing.Point(13, 28);
+            this.lbRodada.Name = "lbRodada";
+            this.lbRodada.Size = new System.Drawing.Size(51, 13);
+            this.lbRodada.TabIndex = 0;
+            this.lbRodada.Text = "Rodada1";
+            // 
+            // lbVitorias
+            // 
+            this.lbVitorias.AutoSize = true;
+            this.lbVitorias.Location = new System.Drawing.Point(13, 61);
+            this.lbVitorias.Name = "lbVitorias";
+            this.lbVitorias.Size = new System.Drawing.Size(50, 13);
+            this.lbVitorias.TabIndex = 1;
+            this.lbVitorias.Text = "Vitórias:0";
+            // 
+            // lbEmpates
+            // 
+            this.lbEmpates.AutoSize = true;
+            this.lbEmpates.Location = new System.Drawing.Point(13, 95);
+            this.lbEmpates.Name = "lbEmpates";
+            this.lbEmpates.Size = new System.Drawing.Size(57, 13);
+            this.lbEmpates.TabIndex = 2;
+            this.lbEmpates.Text = "Empates:0";
+            // 
+            // btPedra
+            // 
+            this.btPedra.Location = new System.Drawing.Point(12, 156);
+            this.btPedra.Name = "btPedra";
+            this.btPedra.Size = new System.Drawing.Size(75, 23);
+            this.btPedra.TabIndex = 4;
+            this.btPedra.Text = "Pedra";
+            this.btPedra.UseVisualStyleBackColor = true;
+            this.btPedra.Click += new System.EventHandler(this.btPedra_Click);
+            // 
+            // btPapel
+            // 
+            this.btPapel.Location = new System.Drawing.Point(93, 156);
+            this.btPapel.Name = "btPapel";
+            this.btPapel.Size = new System.Drawing.Size(124, 23);
+            this.btPapel.TabIndex = 5;
+            this.btPapel.Text = "Papel";
+            this.btPapel.UseVisualStyleBackColor = true;
+            this.btPapel.Click += new System.EventHandler(this.btPapel_Click);
+            // 
+            // btTesoura
+            // 
+            this.btTesoura.Location = new System.Drawing.Point(223, 156);
+            this.btTesoura.Name = "btTesoura";
+            this.btTesoura.Size = new System.Drawing.Size(75, 23);
+            this.btTesoura.TabIndex = 6;
+            this.btTesoura.Text = "Tesoura";
+            this.btTesoura.UseVisualStyleBackColor = true;
+            this.btTesoura.Click += new System.EventHandler(this.btTesoura_Click);
+            // 
+            // rbResultado
+            // 
+            this.rbResultado.Location = new System.Drawing.Point(16, 276);
+            this.rbResultado.Name = "rbResultado";
+            this.rbResultado.Size = new System.Drawing.Size(282, 96);
+            this.rbResultado.TabIndex = 7;
+            this.rbResultado.Text = "";
+            // 
+            // btJogar
+            // 
+            this.btJogar.Location = new System.Drawing.Point(16, 228);
+            this.btJogar.Name = "btJogar";
+            this.btJogar.Size = new System.Drawing.Size(75, 23);
+            this.btJogar.TabIndex = 8;
+            this.btJogar.Text = "Jogar";
+            this.btJogar.UseVisualStyleBackColor = true;
+            this.btJogar.Click += new System.EventHandler(this.btJogar_Click);
+            // 
+            // btDescelecionar
+            // 
+            this.btDescelecionar.Location = new System.Drawing.Point(97, 228);
+            this.btDescelecionar.Name = "btDescelecionar";
+            this.btDescelecionar.Size = new System.Drawing.Size(120, 23);
+            this.btDescelecionar.TabIndex = 9;
+            this.btDescelecionar.Text = "Descelecionar Jogada";
+            this.btDescelecionar.UseVisualStyleBackColor = true;
+            this.btDescelecionar.Click += new System.EventHandler(this.btDescelecionar_Click);
+            // 
+            // btReinicar
+            // 
+            this.btReinicar.Location = new System.Drawing.Point(223, 228);
+            this.btReinicar.Name = "btReinicar";
+            this.btReinicar.Size = new System.Drawing.Size(75, 23);
+            this.btReinicar.TabIndex = 10;
+            this.btReinicar.Text = "Reiniciar";
+            this.btReinicar.UseVisualStyleBackColor = true;
+            this.btReinicar.Click += new System.EventHandler(this.btReinicar_Click);
+            // 
+            // lbJogadaSelecionada
+            // 
+            this.lbJogadaSelecionada.AutoSize = true;
+            this.lbJogadaSelecionada.Location = new System.Drawing.Point(13, 198);
+            this.lbJogadaSelecionada.Name = "lbJogadaSelecionada";
+            this.lbJogadaSelecionada.Size = new System.Drawing.Size(148, 13);
+            this.lbJogadaSelecionada.TabIndex = 11;
+            this.lbJogadaSelecionada.Text = "Nenhuma jogada selecionada";
+            // 
+            // lbDerrotas
+            // 
+            this.lbDerrotas.AutoSize = true;
+            this.lbDerrotas.Location = new System.Drawing.Point(13, 126);
+            this.lbDerrotas.Name = "lbDerrotas";
+            this.lbDerrotas.Size = new System.Drawing.Size(56, 13);
+            this.lbDerrotas.TabIndex = 12;
+            this.lbDerrotas.Text = "Derrotas:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 384);
+            this.Controls.Add(this.lbDerrotas);
+            this.Controls.Add(this.lbJogadaSelecionada);
+            this.Controls.Add(this.btReinicar);
+            this.Controls.Add(this.btDescelecionar);
+            this.Controls.Add(this.btJogar);
+            this.Controls.Add(this.rbResultado);
+            this.Controls.Add(this.btTesoura);
+            this.Controls.Add(this.btPapel);
+            this.Controls.Add(this.btPedra);
+            this.Controls.Add(this.lbEmpates);
+            this.Controls.Add(this.lbVitorias);
+            this.Controls.Add(this.lbRodada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRodada;
+        private System.Windows.Forms.Label lbVitorias;
+        private System.Windows.Forms.Label lbEmpates;
+        private System.Windows.Forms.Button btPedra;
+        private System.Windows.Forms.Button btPapel;
+        private System.Windows.Forms.Button btTesoura;
+        private System.Windows.Forms.RichTextBox rbResultado;
+        private System.Windows.Forms.Button btJogar;
+        private System.Windows.Forms.Button btDescelecionar;
+        private System.Windows.Forms.Button btReinicar;
+        private System.Windows.Forms.Label lbJogadaSelecionada;
+        private System.Windows.Forms.Label lbDerrotas;
+    }
+}
+
---
+++ CorrecaDaProva/CorrecaDaProva/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorrecaDaProva
+{
+    public partial class Form1 : Form
+    {   
+        /*
+         * Valor da jogada Selecionada
+         * 0 = nenhuma jogada
+         * 1= pedra
+         * 2= papel
+         * 3=tesoura
+         */
+         
+
+        int jogadaSelecionada = 0;
+
+        string[] jogadas = new string[3]
+        {
+            "Pedra",
+            "Papel",
+            "Tesoura"
+        };
+
+        int rodada = 1;
+        int vitorias = 0;
+        int empates = 0;
+        int derrotas = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btPedra_Click(object sender, EventArgs e)
+        {
+            jogadaSelecionada = 1;
+            AtualizarJogadaSelecionada();
+
+        }
+
+        private void AtualizarJogadaSelecionada()
+        {
+
+            if (ChecarJogadaValida())
+            {
+                lbJogadaSelecionada.Text = string.Format(
+                    "Você selecionou a jogada{0}.",
+                    jogadas[jogadaSelecionada - 1]
+                    ); 
+            }
+            else
+            {
+                lbJogadaSelecionada.Text = "Nenhuma jogada seleceionada.";
+            }
+        }
+
+        private bool ChecarJogadaValida()
+        {
+            return jogadaSelecionada > 0;
+        }
+
+        private void btJogar_Click(object sender, EventArgs e)
+        {
+            if (ChecarJogadaValida())
+            {
+                Jogar();
+            }
+
+        }
+
+        /*enum Jogadas
+        {
+            PEDRA =1;
+            PAPEL =2;
+            TESOURA =3
+        }
+        */
+
+        private void Jogar()
+        {
+            Random rnd = new Random();
+            int jogadaComputador = rnd.Next(1, jogadas.Length+1);
+
+            ExibirJogada("Jogador", jogadaSelecionada);
+            ExibirJogada("Computador", jogadaComputador);
+
+            //jogada sekecionada = Pedra
+            // if(jogadaSelecionada == Jogadas.PEDRA )
+            if (jogadaSelecionada==1)
+            {
+                if (jogadaComputador ==1)
+                {
+                    Empate();
+
+
+                }
+                else if (jogadaComputador==2)
+                {
+                    Derrota();
+                }
+                else if (jogadaComputador == 3)
+                {
+                    Vitoria();
+                }
+            }
+            if (jogadaSelecionada == 2)
+            {
+                if (jogadaComputador == 1)
+                {
+                    Empate();
+
+                }
+                else if (jogadaComputador == 2)
+                {
+                    Derrota();
+                }
+                else if (jogadaComputador == 3)
+                {
+                    Vitoria();
+                }
+
+            }
+            if (jogadaSelecionada == 3)
+            {
+                if (jogadaComputador == 1)
+                {
+                    Empate();
+
+
+                }
+                else if (jogadaComputador == 2)
+                {
+                    Derrota();
+                }
+                else if (jogadaComputador == 3)
+                {
+                    Vitoria();
+                }
+            }
+
+
+            AvancarRodada();
+
+        }
+
+        private void ExibirJogada(string nomeJogador, int jogada)
+        {
+            rbResultado.AppendText(
+                string.Format(
+                    "{0}:{1}\n",
+                    nomeJogador,
+                    jogadas[jogada - 1])
+                    );
+        }
+
+        private void Vitoria()
+        {
+            vitorias++;
+
+            rbResultado.AppendText("Você ganhou.\n");
+
+           
+
+        }
+
+        private void AvancarRodada()
+        {
+            DesselecionarJogada();
+            rodada++;
+            AtualizarLabelsJogo();
+        }
+
+        private void DesselecionarJogada()
+        {
+            jogadaSelecionada = 0;
+            AtualizarJogadaSelecionada();
+        }
+
+        private void AtualizarLabelsJogo()
+        {
+            lbRodada.Text = "Rodada:" + rodada;
+            lbVitorias.Text = "Rodada:" + vitorias;
+            lbEmpates.Text = "Rodada:" + empates;
+            lbDerrotas.Text = "Rodada:" + derrotas;
+        }
+
+        private void Derrota()
+        {
+            derrotas++;
+
+            rbResultado.AppendText("Você perdeu.\n");
+
+
+
+
+        }
+
+        private void Empate()
+        {
+            empates++;
+
+            rbResultado.AppendText("Você empatou.\n");
+
+
+
+        }
+
+        private void btPapel_Click(object sender, EventArgs e)
+        {
+            jogadaSelecionada = 2;
+            AtualizarJogadaSelecionada();
+        }
+
+        private void btTesoura_Click(object sender, EventArgs e)
+        {
+            jogadaSelecionada = 3;
+            AtualizarJogadaSelecionada();
+        }
+
+        private void btDescelecionar_Click(object sender, EventArgs e)
+        {
+            DesselecionarJogada();
+        }
+
+        private void btReinicar_Click(object sender, EventArgs e)
+        {
+            rodada = 1;
+            vitorias = 0;
+            empates = 0;
+            derrotas = 0;
+            AtualizarLabelsJogo();
+
+            DesselecionarJogada();
+
+            rbResultado.Clear();
+        }
+    }
+}
